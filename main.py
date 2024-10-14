30e4444a9c3343c978fde70d2ac570af3cc89807
dcicio <dcicio@gmail.com>
2024-10-14 13:18:03 -0400

    Second commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# placeholder text
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+	print("Starting asteroids!")
+
+if __name__ == "__main__":
+	main()
