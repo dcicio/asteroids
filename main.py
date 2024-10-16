7d22670d81fd99b01c843554d2564a2c51c8b2a1
dcicio <dcicio@gmail.com>
2024-10-16 14:46:11 -0400

    Setting up FPS in the game loop

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ from constants import *
 
 def main():
 	pygame.init()
+	timer = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
+	# GAME LOOP
 	while True:
-		for event in pygame.event.get():
+		for event in pygame.event.get():		# Closing the window will exit the game loop
 			if event.type == pygame.QUIT:
 				return
-		screen.fill(000000)
+		screen.fill("black")
 		pygame.display.flip()
+		dt = timer.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
