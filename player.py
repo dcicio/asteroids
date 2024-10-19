56208d4782f4785d45c9bf7e15558a7b3f600c62
dcicio <dcicio@gmail.com>
2024-10-19 13:37:30 -0400

    Added PLAYER_SPEED constant. Added forward and backward movement

--- player.py
+++ player.py
@@ -18,6 +18,23 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2) # Draw to screen, in white, using triangle draw, with line width 2
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
 
-def main():
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        if keys[pygame.K_a]:            # Rotate left
+            self.rotate(-1 * dt)
+        if keys[pygame.K_d]:            # Rotate right
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-1 * dt)
+    
+    def move(self, dt):                                         # 1. We start with a unit vector pointing straight up from (0, 0) to (0, 1).
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)    # 2. We rotate that vector by the player's rotation, so it's pointing in the direction the player is facing.
+        self.position += forward * PLAYER_SPEED * dt            # 3. We multiply by PLAYER_SPEED * dt. A larger vector means faster movement.
+                                                                # 4. Add the vector to our position to move the player.
